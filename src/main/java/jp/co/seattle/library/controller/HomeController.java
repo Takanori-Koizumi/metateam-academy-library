1991ef4c6169109674919782a3d5fbda31966209
小泉崇悟 <>
2024-01-29 20:26:34 +0900

    fix 呼び出しのない変数削除。

--- src/main/java/jp/co/seattle/library/controller/HomeController.java
+++ src/main/java/jp/co/seattle/library/controller/HomeController.java
@@ -2,13 +2,10 @@ package jp.co.seattle.library.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import jp.co.seattle.library.service.BooksService;
 
 /**
  * Handles requests for the application home page.
@@ -16,9 +13,6 @@ import jp.co.seattle.library.service.BooksService;
 @Controller // APIの入り口
 public class HomeController {
 	final static Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-	@Autowired
-	private BooksService booksService;
 
 	/**
 	 * Homeボタンからホーム画面に戻るページ
